b3e11b5adc51953e3cc83656584cff33995b70d6
lenaf <lforti41@gmail.com>
2024-06-25 14:22:13 -0400

    carousel tweaks

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -10,6 +10,8 @@ interface CarouselProps extends React.HTMLProps<HTMLDivElement> {
 export function Carousel({ className, children, id, isAutoPlay = true, showArrows, ...rest }: CarouselProps) {
     const [activeIndex, setActiveIndex] = useState<number>(0);
     const itemsLength = Children.count(children);
+    const isFirstActive = activeIndex === 0;
+    const isLastActive = activeIndex === itemsLength - 1;
 
     const scrollToIndex = (index: number) => {
         const carousel = document.getElementById(id);
@@ -20,13 +22,13 @@ export function Carousel({ className, children, id, isAutoPlay = true, showArrow
     };
 
     const scrollForward = () => {
-        const newActiveIndex = activeIndex + 1 >= itemsLength ? 0 : activeIndex + 1;
+        const newActiveIndex = isLastActive ? 0 : activeIndex + 1;
         scrollToIndex(newActiveIndex);
         setActiveIndex(newActiveIndex);
     };
 
     const scrollBack = () => {
-        const newActiveIndex = activeIndex < 1 ? itemsLength - 1 : activeIndex - 1;
+        const newActiveIndex = isFirstActive ? itemsLength - 1 : activeIndex - 1;
         scrollToIndex(newActiveIndex);
         setActiveIndex(newActiveIndex);
     };
@@ -41,19 +43,31 @@ export function Carousel({ className, children, id, isAutoPlay = true, showArrow
     // }, [activeIndex]);
 
     return (
-        <div
-            {...rest}
-            id={id}
-            className={`carousel w-full ${className}`}
-        >
-            {Children.map(children, (child) =>
-                <div className='carousel-item relative w-full'>
-                    {child}
-                    <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-                        <Button onClick={() => scrollBack()} className="btn-circle">❮</Button>
-                        <Button onClick={() => scrollForward()} className="btn-circle">❯</Button>
-                    </div>
-                </div>)}
+        <div className={`relative w-full ${className}`}>
+            <div
+                {...rest}
+                id={id}
+                className="carousel relative w-full"
+            >
+                {Children.map(children, (child) =>
+                    <div className='carousel-item w-full'>
+                        {child}
+                    </div>)}
+            </div>
+            <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+                <Button
+                    onClick={() => scrollBack()}
+                    className={`btn-circle ${isFirstActive ? 'invisible' : ''}`}
+                >
+                    ❮
+                </Button>
+                <Button
+                    onClick={() => scrollForward()}
+                    className={`btn-circle ${isLastActive ? 'invisible' : ''}`}
+                >
+                    ❯
+                </Button>
+            </div>
         </div>
     )
 }
