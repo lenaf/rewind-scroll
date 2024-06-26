906b3478d1da89630e78415f4e9e94c69f9de6fd
lenaf <lforti41@gmail.com>
2024-06-26 18:38:35 -0400

    calendar sizing

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -5,10 +5,7 @@ export const metadata = genPageMetadata({ title: 'Events' })
 
 export default function Page() {
   return (
-    <div>
-      <div className="prose max-w-sm mb-4">
-        <h1>Events Calendar</h1>
-      </div>
+    <div className="p-12">
       <Calendar />
     </div>
   )
