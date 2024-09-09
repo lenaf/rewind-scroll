58abce6f210b1b0ff71751ce9236b80b4b769626
lenaf <lforti41@gmail.com>
2024-09-08 20:21:20 -0400

    fix nav style

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,26 +6,28 @@ import LinkAsButton from './common/LinkAsButton';
 
 const Header = () => {
   return (
-    <header className="bg-gradient-to-b from-base-100 from-10% to-transparent absolute sticky top-0 w-full z-10 flex items-center justify-between py-4 px-4 uppercase">
-      <div>
-        <Link href="/" >
-          <Logo width={80} />
-        </Link>
-      </div>
-      <div className='desktop'>
-        <div className="flex items-center space-x-4 leading-5 sm:space-x-6">
-          {siteData.routes.map((link) => (
-            <Link
-              key={link.title}
-              href={link.href}
-            >
-              {link.title}
-            </Link>
-          ))}
-          <LinkAsButton className="btn-primary " href="/" target="_blank">Trailer</LinkAsButton>
+    <header className="-mb-24 bg-gradient-to-b from-base-100 from-10% to-transparent sticky top-0 w-full z-10">
+      <div className='h-24 px-8 flex items-center justify-between uppercase'>
+        <div>
+          <Link href="/" >
+            <Logo width={80} />
+          </Link>
         </div>
+        <div className='desktop'>
+          <div className="flex items-center space-x-4 leading-5 sm:space-x-6">
+            {siteData.routes.map((link) => (
+              <Link
+                key={link.title}
+                href={link.href}
+              >
+                {link.title}
+              </Link>
+            ))}
+            <LinkAsButton className="btn-primary " href="/" target="_blank">Trailer</LinkAsButton>
+          </div>
+        </div>
+        <MobileNav />
       </div>
-      <MobileNav />
     </header>
   )
 }
