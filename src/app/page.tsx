8e416161de3a867cac7f189018806c6a379b0d91
lena.forti <lena.forti@disneystreaming>
2024-10-16 11:59:06 -0400

    try to fix mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
       <div style={{ height: '100vh', position: 'relative' }}>
         <PageScroller>
           {[0, 1, 2, 3, 4, 5].map((i) => {
-            return (<div style={{ background: getRandomColor(), padding: '400px', height: '100vh', width: '1000%', display: 'flex', fontSize: '64px' }} key={i} >
+            return (<div style={{ background: getRandomColor(), padding: '400px 100px', height: '100vh', width: '1000%', display: 'flex', fontSize: '64px' }} key={i} >
               <h1>Page {i + 1}</h1>
             </div>);
           })}
